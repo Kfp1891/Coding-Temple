09733a79d5d8356cc7b03977d4481c1adf6e53b6
Robert Burrows <robertb400@comcast.net>
2021-08-01 14:42:31 -0700

    first commit

---
+++ preworkQ1.py
@@ -0,0 +1,6 @@
+#Write a function to print "hello_USERNAME!" USERNAME is the input of the function. 
+#The first line of the code has been defined as below.
+def hello_username (username):
+        return f"hello_username, {username}"
+username = ("Robert")
+print (f"hello_username:", username)
